99dd161674e75a0f1ae610f04563eccc50a9dd4f
nivet <fun5xxx@gmail.com>
2022-01-13 16:54:39 +0800

    Initial commit

---
+++ manager/public/config.js
@@ -0,0 +1,34 @@
+var BASE = {
+    /**
+     * @description api请求基础路径
+     */
+    API_DEV: {
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8889",
+      manager: "http://localhost:8887"
+    },
+  /**
+   * todo: 修改
+   */
+  API_PROD: {
+      common: "https://common-api.pickmall.cn",
+      buyer: "https://buyer-api.pickmall.cn",
+      seller: "https://store-api.pickmall.cn",
+      manager: "https://admin-api.pickmall.cn"
+    },
+    /**
+     * @description // 跳转买家端地址 pc端
+     * todo: 修改
+     */
+    PC_URL: "https://pc-b2b2c.pickmall.cn",
+    /**
+     * @description  // 跳转买家端地址 wap端
+     * todo: 修改
+     */
+    WAP_URL: "https://m-b2b2c.pickmall.cn",
+    /**
+     *  @description api请求基础路径前缀
+     */
+    PREFIX: "/manager"
+  };
---
+++ manager/src/config/index.js
@@ -0,0 +1,16 @@
+export default {
+  /**
+   * @description 配置显示在浏览器标签的title
+   */
+  title: "sftgshop",
+  /**
+   * @description 高德web端申请的api key
+   */
+  aMapKey: "289ae01affd16014dd9035d5cb56a636",
+  aMapSecret: "d8224e975c77ca510682a119a94ac3fc",
+  /**
+   * @description 官网地址
+   * todo: 修改
+   */
+  website: "https://www.pickmall.cn",
+};
